519911bde212895131f9f6e52cf3f3af933921e1
Sebastian Carrera <sebascarreram@hotmail.com>
2020-04-05 03:32:24 -0500

    routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,21 +1,35 @@
 const express = require('express');
 
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 // ROUTES
 router
   .route('/')
-  .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .get(authController.protect, tourController.getAllTours)
+  .post(tourController.createTour);
 
 router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.udpateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    // If user have jwt or password has changed or user don't exist'
+    authController.protect,
+    // Only for admin, lead-guide to be able to delete tour here
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,51 @@
 const express = require('express');
 
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 // ROUTES
+
+// SIGNUP
+// router.route('/signup').post(authController.signup);
+// When there is only a post, it can use only .post() without .route() so like down:
+router.post('/signup', authController.signup);
+//
+// LOGIN
+router.post('/login', authController.login);
+
+// Forgot Password
+// Which will only receive the email address
+router.post('/forgotPassword', authController.forgotPassword);
+
+// Reset Password
+// Which will receive the token as well as the new password
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Updating Password
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+// Updating my data personal
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+// It also keep these routes here down so the ones in a more REST format
+// Because there's also the possibility of a system administrator updating or
+// deleting or getting all users based on their ID.
 router
   .route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+  .get(userController.getAllUsers) // Get all users
+  .post(userController.createUser); // Create a user
 
 router
   .route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
+  .get(userController.getUser) // Get an users by ID
+  .patch(userController.updateUser) // Update a user by ID
+  .delete(userController.deleteUser); // Delete a user by ID
 
 module.exports = router;
