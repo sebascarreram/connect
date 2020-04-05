e0f1eec3aa1215bce9b4b74e43903e382d07bb1b
Sebastian Carrera <sebascarreram@hotmail.com>
2020-04-05 03:35:29 -0500

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
+
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
-// 1) MIDDLEWARE
+// 1) GLOBAL MIDDLEWARE
 //
 // 'dev' -> Concise output colored by response status for development use.
 // status >= 500 // red
@@ -25,19 +36,61 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // ⬆️
 }
-//
-// for application/json
-app.use(express.json());
 
+const limiter = rateLimit({
+  // 100 requests from the same IP in 1 hour
+  max: 100,
+  // window Milliseconds
+  // 1 hour
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again later in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body int req.body
+// for application/json
+// app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization againist NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization againist XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-// Test, how to works 'next()'
+// Test middleware
 app.use((req, res, next) => {
-  console.log('Hello from middleware ♥️');
-  // next() => Next middleware after execut here, not problem BUT if there's error or missing something.
-  // It should to be a 'return'  with or without callback
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers); // will show authorization and another more
+  // console.log(x);
   next();
 });
+
+///////// Test, how to works 'next()'
+/////
+// app.use((req, res, next) => {
+//   console.log('Hello from middleware ⬇️');
+//   // next() => Next middleware after execut here, not problem BUT if there's error or missing something.
+//   // It should to be a 'return'  with or without callback
+//   next();
+// });
 
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
@@ -107,5 +160,38 @@ app.use((req, res, next) => {
 //
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  ////// There is three(3) ways for response Error
+  // 1)
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!!.`
+  // });
+  //
+  // 2)
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!!.`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  // next(err);
+  //
+  // 3)
+  // AppError is a file called appError.js in 'utils' folder
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!.`, 404));
+});
+
+// GLOBAL ERROR
+// app.use((err, req, res, next) => {
+//   console.log(err.stack);
+
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'error';
+
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message
+//   });
+// });
+app.use(globalErrorHandler);
 
 module.exports = app;
